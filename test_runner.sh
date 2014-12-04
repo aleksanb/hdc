c33dfc14508a83892714cdb6a6954d51863f17cf
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-12-04 02:30:13 +0100

    Such improve. Very runner!

--- test_runner.sh
+++ test_runner.sh
@@ -3,14 +3,28 @@
 rm -rf test_output
 mkdir test_output
 
+red='\033[0;31m'
+blue='\e[0;34m'
+clr='\033[0m'
+green='\e[0;32m'
+
+echo -e "Starting HDC testrunner.";
 for program in $(ls source_programs/*.d); do
-  echo "Testing $program ...";
+  echo -e "${green}Testing $program ...${red}";
 
   basename=$(basename $program .d)
   program_diff=$basename.diff
   program_compiled=$basename.kernel
 
   ./dist/build/hdc/hdc < $program > test_output/$program_compiled
-  diff test_output/$program_compiled correct_output/$program_compiled > test_output/$program_diff
+
+  if [ -f correct_output/$program_compiled ]; then
+    echo -e "${clr}Generated diff for $program_compiled."
+    diff test_output/$program_compiled correct_output/$program_compiled > test_output/$program_diff
+  else
+    echo -e "${red}Correct output not found for $program_compiled, skipping diff.${clr}"
+  fi
+
+  echo
 done;
 

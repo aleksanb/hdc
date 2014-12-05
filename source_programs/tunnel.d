ab6924d8fe3cbf5f6249a9f014711f25956b1a32
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-12-05 19:08:40 +0100

    Fix syntax in tunnel.d

--- source_programs/tunnel.d
+++ source_programs/tunnel.d
@@ -4,19 +4,18 @@ y = $id_low >> 6
 edge_offset = $constants[10]
 other_edge_offset = 64 - edge_offset
 
-mask |=
+$mask |=
   x in [edge_offset, other_edge_offset]
-  && edge_offset < y < other_edge_offset
+  and edge_offset < y < other_edge_offset
 
 $mask |=
   y in [edge_offset, other_edge_offset]
   and edge_offset < x < other_edge_offset
 
 $mask |=
-  x == y
-  or x == -y
+  x in [y, -y]
 
 $address = $id
-$data = $mask ? 0xffff : `rgb(0, 0, 255)`
+$data = $mask ? 0xffff : 0xeeee #`rgb(0, 0, 255)`
 
 store!

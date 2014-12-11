d9b01f720d341697b344c7781b05f6ae1082344c
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-12-11 19:19:18 +0100

    Add RegisterAllocation skeleton

--- src/Optimizer.hs
+++ src/Optimizer.hs
@@ -1,7 +1,9 @@
 module Optimizer(optimize) where
 
 import qualified Optimizer.Constants as Constants
+import qualified Optimizer.RegisterAllocation as RegisterAllocation
 import Datatypes
 
 optimize :: [IR] -> [IR]
-optimize ir = Constants.optimize ir
+optimize ir = (RegisterAllocation.optimize
+ . Constants.optimize) ir
---
+++ src/Optimizer/RegisterAllocation.hs
@@ -0,0 +1,6 @@
+module Optimizer.RegisterAllocation(optimize) where
+
+import Datatypes
+
+optimize :: [IR] -> [IR]
+optimize ir = ir

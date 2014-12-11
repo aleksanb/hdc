7c053b15b7872266039f351546f48d56ba32f1f0
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-12-11 17:23:34 +0100

    Move Constants optimization to submodule

---
+++ src/Optimizer/Constants.hs
@@ -0,0 +1,85 @@
+module Optimizer.Constants(optimize) where
+
+import Datatypes
+import Data.Maybe
+import Data.Bits
+import Control.Monad.State
+import Control.Monad.Fix
+import qualified Data.Map as Map
+
+type Constants = Map.Map Int Int
+
+
+optimize :: [IR] -> [IR]
+optimize ir = map cleanZeros $ fixedPoint ir
+
+
+cleanZeros :: IR -> IR
+cleanZeros ir@(ThreeIR op rd (I i) rt m)
+  | i == 0 = ThreeIR op rd (R 0) rt m
+cleanZeros ir@(ThreeIR op rd rs (I i) m)
+  | i == 0 = ThreeIR op rd rs (R 0) m
+cleanZeros other = other
+
+
+fixedPoint ir
+  | ir == ir' = ir
+  | otherwise = optimize ir'
+  where ir' = evalState (propagateConstants ir) (Map.fromList [(0, 0)])
+
+propagateConstants :: [IR] -> State Constants [IR]
+propagateConstants ir = do
+  mapM (propagateConstant) ir
+
+
+propagateConstant :: IR -> State Constants IR
+propagateConstant ir@(TwoIR (R r1) (I i1) m) = do
+  constants <- get
+  put $ Map.insert r1 i1 constants
+  return ir
+
+propagateConstant (ThreeIR op (R r1) (I i1) (I i2) m) = do
+  let newImmediate = operandFor op i1 i2
+      newIR = TwoIR (R r1) (I newImmediate) m
+
+  constants <- get
+  put $ Map.insert r1 newImmediate constants
+  return newIR
+
+propagateConstant (ThreeIR op (R r1) r2 r3 mask) = do
+  r2' <- getFor r2
+  r3' <- getFor r3
+
+  constants <- get
+  put $ Map.delete r1 constants
+  return $ ThreeIR op (R r1) r2' r3' mask
+
+propagateConstant other = do return other
+
+
+getFor :: IRItem -> State Constants IRItem
+getFor (R reg) = do
+  constants <- get
+  return (case Map.lookup reg constants of
+            Just constant -> I constant
+            _ -> R reg)
+
+getFor other = do return other
+
+operandFor :: BinaryOp -> Int -> Int -> Int
+operandFor op =
+  case op of
+    And -> (.&.)
+    BitwiseAnd -> (.&.)
+    Or -> (.|.)
+    BitwiseOr -> (.|.)
+    BitwiseXor -> xor
+    Plus -> (+)
+    Minus -> (-)
+    Multiply -> (*)
+    ShiftLeft -> shiftL
+    ShiftRight -> rotateR
+    ShiftRightArithmetic -> shiftR
+    rqualTo -> \a b -> fromEnum  (a == b)
+    LessThan -> \a b -> fromEnum (a < b)
+    GreaterThan -> \a b -> fromEnum (a >= b)

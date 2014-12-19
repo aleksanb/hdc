dc6fc2be666cfcd04192030d29076d0d167e069e
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-12-19 21:39:36 +0100

    Fix bug in liveness when same operand appears twice in righthand

--- src/Optimizer/Dataflow.hs
+++ src/Optimizer/Dataflow.hs
@@ -2,26 +2,28 @@ module Optimizer.Dataflow(getLiveVariables) where
 
 import Datatypes
 import Data.List
+import Debug.Trace
 
 
 getLiveVariables :: [IR] -> [[Int]]
 getLiveVariables ir =
-  let live = foldr analyseLiveness [[]] ir
-      shiftedLive = tail live ++ [[]] in 
   map (uncurry union) (zip live shiftedLive)
-
+  where live = foldr analyseLiveness [[]] ir
+        shiftedLive = tail live ++ [[]]
 
 analyseLiveness :: IR -> [[Int]] -> [[Int]]
-analyseLiveness (ThreeIR _ r1 r2 r3 _) (live:lives) =
-  let gen = (toI r2) ++ (toI r3)
-      kill = toI r1
-      live' = gen `union` (live \\ kill)
-  in live':live:lives
+analyseLiveness (ThreeIR _ r1 r2 r3 masked) (live:lives)
+  | masked = live:live:lives
+  | otherwise =
+    live':live:lives
+  where gen = (toI r2) `union` (toI r3)
+        kill = toI r1
+        live' = gen `union` (live \\ kill)
 
-analyseLiveness (TwoIR r1 _ _) (live:lives) =
-  let kill = toI r1
-      live' = live \\ kill
-  in live':live:lives
+analyseLiveness (TwoIR r1 _ masked) (live:lives)
+  | masked = live:live:lives
+  | otherwise = live':live:lives
+  where live' = live \\ (toI r1)
 
 analyseLiveness _ lives = lives
 

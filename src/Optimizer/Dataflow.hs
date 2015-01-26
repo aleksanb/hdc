7e8ab4b1aeefa403d18ea2e02af2b23873d3e1ad
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2015-01-26 15:56:20 +0100

    Correctly handle masked instructions in live variable analysis

--- src/Optimizer/Dataflow.hs
+++ src/Optimizer/Dataflow.hs
@@ -12,18 +12,17 @@ getLiveVariables ir =
         shiftedLive = tail live ++ [[]]
 
 analyseLiveness :: IR -> [[Int]] -> [[Int]]
-analyseLiveness (ThreeIR _ r1 r2 r3 masked) (live:lives)
-  | masked = live:live:lives
-  | otherwise =
-    live':live:lives
+analyseLiveness (ThreeIR _ r1 r2 r3 masked) rest@(liveOut:lives)
+  | masked = liveInMasked:rest
+  | otherwise = liveIn:rest
   where gen = (toI r2) `union` (toI r3)
-        kill = toI r1
-        live' = gen `union` (live \\ kill)
+        liveInMasked = gen `union` liveOut -- Masked instructions only generate liveness
+        liveIn = gen `union` (liveOut \\ (toI r1))
 
-analyseLiveness (TwoIR r1 _ masked) (live:lives)
-  | masked = live:live:lives
-  | otherwise = live':live:lives
-  where live' = live \\ (toI r1)
+analyseLiveness (TwoIR r1 _ masked) rest@(liveOut:lives)
+  | masked = liveOut:rest -- Masked instructions don't kill variables
+  | otherwise = liveIn:rest
+  where liveIn = liveOut \\ (toI r1)
 
 analyseLiveness _ lives = lives
 

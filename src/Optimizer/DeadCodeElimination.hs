576cf809016d1e3ad8ce49f6b6243c5fc0e4cbfb
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2015-02-02 04:07:09 +0100

    Rewrite parts of dce for readability

--- src/Optimizer/DeadCodeElimination.hs
+++ src/Optimizer/DeadCodeElimination.hs
@@ -7,17 +7,21 @@ import Data.List
 
 optimize :: [IR] -> Writer [String] [IR]
 optimize ir = do
-  let prunedIR = fixedPoint (\ir -> let liveVariables = foldl union [] (getLiveVariables ir) in filter (keepInstruction liveVariables) ir) ir
+  let prunedIR =
+        fixedPoint
+          (\ir ->
+            let liveVariables = foldl union [] (getLiveVariables ir)
+            in filter (keepInstruction liveVariables) ir)
+          ir
 
   tell [ show (length ir - length prunedIR) ++ " statements out of " ++ show (length ir) ++ " found dead." ]
   return prunedIR
 
-
 keepInstruction :: [Int] -> IR -> Bool
-keepInstruction liveVars (ThreeIR _ (R target) _ _ _)
-  | target >= 7 = target `elem` liveVars
-
-keepInstruction liveVars (TwoIR (R target) _ _)
-  | target >= 7 = target `elem` liveVars
-
-keepInstruction liveVars others = True
+keepInstruction liveVars ir
+  | r >= 7 = r `elem` liveVars
+  | otherwise = True
+  where r = case ir of
+              ThreeIR _ (R target) _ _ _ -> target
+              TwoIR (R target) _ _ -> target
+              _ -> 0 -- To keep the other IR types

5b2113b521a1fb8b6374e031d8b4ef77956e6826
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2015-10-22 00:32:22 +0200

    Add Assembler module
    This module implements the functions needed to have hexcode as compile
    target. All functions have been tested manually in ghci.

---
+++ src/Assembler.hs
@@ -0,0 +1,42 @@
+module Assembler(assemble) where
+
+import Datatypes
+import Opcodes
+
+assemble :: [IR] -> [Int]
+assemble instructions = (map assembleInstruction instructions)
+
+assembleInstruction :: IR -> Int
+
+assembleInstruction LoadIR = (hexOpcode Lw)
+
+assembleInstruction StoreIR = (hexOpcode Sw)
+
+assembleInstruction (TwoIR (R reg) (I immediate)  masked) =
+  (hexMask masked) + (hexOpcode Addi) + (hexRt reg) + (immediate `mod` 2**16)
+
+assembleInstruction (TwoIR (R reg) (C address) masked) =
+  (hexMask masked) + (hexOpcode Ldc) + (hexRt reg) + (address `mod` 2**16)
+
+assembleInstruction (ThreeIR operator (R rd) (R rs) (R rt) masked) =
+  (hexMask masked) + (hexOpcode opcode) + (hexRd rd) + (hexRs rs) + (hexRt rt) + (hexAlufunction opcode)
+    where opcode = case operator of
+                BitwiseAnd -> And
+                BitwiseOr -> Or
+                BitwiseXor -> Xor
+                Plus -> Add
+                Minus -> Sub
+                Multiply -> Mul
+                LessThan -> Slt
+                EqualTo -> Seq
+
+assembleInstruction (ThreeIR op rd (I rs) (R rt) masked)
+  | op == Plus = assembleInstruction (ThreeIR op rd (R rt) (I rs) masked)
+
+assembleInstruction (ThreeIR operator (R rd) (R rs) (I sh) masked) =
+  (hexMask masked) + (hexOpcode opcode) + (hexRd rd) + (hexRs rs) + sh
+  where opcode = case operator of
+               Plus -> Addi
+               ShiftLeft -> Sll
+               ShiftRight -> Srl
+               ShiftRightArithmetic -> Sra
